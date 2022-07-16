3e2bf417b149e34b1861956d9a132c9ba63f8fee
vvzvlad <vvzvlad@gmail.com>
2022-07-16 23:31:16 +0300

    Auto(press enter): 2022-07-16 23:31:16

--- libraries/framebuffer/framebuffer.h
+++ libraries/framebuffer/framebuffer.h
@@ -3,7 +3,8 @@
 class  aFrameBuffer : public Adafruit_GFX {
   public:
     aFrameBuffer(int16_t w, int16_t h, Adafruit_ST7735 *tft, uint8_t spi_dc, uint8_t spi_cs): Adafruit_GFX(w, h);
-		void init();
+
+    void init();
     void drawPixel( int16_t x, int16_t y, uint16_t color);
     void display();
 };
