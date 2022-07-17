747288e436701da93ac03f67fddac901b27cf133
vvzvlad <vvzvlad@gmail.com>
2022-07-18 01:00:51 +0300

    Auto(press enter): 2022-07-18 01:00:51

--- npml1/npml1.ino
+++ npml1/npml1.ino
@@ -4,6 +4,7 @@
 #include <Adafruit_ST7735.h> 				// Hardware-specific library for ST7735
 #include <Adafruit_GFX_Buffer.h>		// Framebuffer library to speed up rendering
 #include <utf8rus.h>
+#include <verdana_bold_12.h>
 
 #include <SPI.h>
 
@@ -43,10 +44,12 @@
 #define GRAPH_HEIGHT_DIVIDER 		20  // 1024(single-count resolution, MAX_ADC_VALUE) ->  50(chart height, GRAPH_HEIGHT)
 #define GRAPH_X 								0
 #define GRAPH_Y 								ST7735_TFT_HEIGHT-GRAPH_HEIGHT
+#define LUMINOFOR_EM_MODE_DEEP 	3
 
 #define CORRECTION_NUM_SAMPLES 	512
 #define MEASURE_NUM_SAMPLES 		512
 #define SYNC_NUM_SAMPLES 				256
+
 #define MAX_ADC_VALUE 					1024
 
 #define MAX_FREQ 								300
@@ -58,8 +61,15 @@ BH1750FVI LightSensor(BH1750FVI::k_DevModeContLowRes);
 EncButton<EB_CALLBACK, BUTTON_PIN> btn(INPUT);
 TickerScheduler ts(5);
 
-uint16_t GLOBAL_adc_correction = 0;
+volatile uint16_t GLOBAL_adc_correction = 0;
 
+volatile uint8_t GLOBAL_flicker_gost_uint = 0;
+volatile uint8_t GLOBAL_flicker_simple_uint = 0;
+volatile uint16_t GLOBAL_freq = 0;
+volatile uint16_t GLOBAL_adc_values_max = 1;
+volatile uint8_t GLOBAL_graph_values[LUMINOFOR_EM_MODE_DEEP][GRAPH_WIDTH] = {};
+
+volatile uint16_t GLOBAL_lum = 0;
 
 void draw_asset(const asset_t *asset, uint8_t x, uint8_t y) {
   uint8_t h = pgm_read_byte(&asset->height);
@@ -85,6 +95,82 @@ uint16_t get_adc_correction_value(uint16_t correction_catch_time_ms) {
 
 	return correction_value;
 }
+
+
+
+void render_flicker_screen() {
+	//uint16_t GLOBAL_freq = freq;
+	//uint16_t GLOBAL_adc_values_max = adc_values_max;
+
+	framebuffer.fillRoundRect(0, 0, 128, 160-GRAPH_HEIGHT, 0, ST7735_TFT_BLACK); //Очистка только избражения над графиком!
+	uint8_t flicker = GLOBAL_flicker_simple_uint; //or GLOBAL_flicker_gost_uint
+
+	if (flicker >= 0 && flicker <= 5) {
+		draw_asset(&flicker_msg_good_lamp, 0, 0);
+	}
+	else if (flicker > 5 && flicker <= 30) {
+		draw_asset(&flicker_msg_normal_lamp, 0, 0);
+	}
+	else if (flicker > 30) {
+		draw_asset(&flicker_msg_bad_lamp, 0, 0);
+	}
+	else {
+		Serial.println((String)"Flicker simple:"+flicker);
+	}
+	draw_asset(&flicker_rainbow, 0, 39);
+	draw_asset(&arrow, 10, 56);
+	draw_asset(&flicker_text_flicker_level, 0, 61);
+
+	framebuffer.setFont(&verdana_bold12pt7b);
+	framebuffer.setCursor(30, 100);
+	framebuffer.setTextSize(1);
+	framebuffer.println((String)flicker+"%");
+	framebuffer.setFont();
+
+
+	framebuffer.setCursor(0, 150);
+	framebuffer.setTextColor(ST7735_TFT_WHITE);
+	framebuffer.setTextSize(1);
+	//framebuffer.println((String)"Flicker simple:"+flicker_simple_uint);
+	//framebuffer.println((String)"Correction:"+GLOBAL_adc_correction);
+	//framebuffer.println((String)"Average:"+adc_values_avg);
+	//framebuffer.println((String)"Max:"+adc_values_max);
+	//framebuffer.println((String)"Min:"+adc_values_min);
+	//framebuffer.println((String)"Tm:	"+((float)catch_time_us/1000)+"ms");
+	framebuffer.println((String)"Freq:"+GLOBAL_freq+" Hz");
+	//framebuffer.println((String)"Light:"+LightSensor.GetLightIntensity()+" lx");
+
+
+
+
+	//Graph render
+
+	for (uint8_t row=GRAPH_Y; row<GRAPH_Y+GRAPH_HEIGHT; row++) {
+    for (uint8_t col=GRAPH_X; col<GRAPH_WIDTH; col++) {
+      framebuffer.drawPixel(col, row, ST7735_TFT_GREEN);
+    }
+  }
+	//uint16_t current_pixel = framebuffer.getPixel(100, 100);
+//
+	//for (uint8_t current_colon = GRAPH_X; current_colon < GRAPH_WIDTH; current_colon++) {
+	//	if (current_colon == 0) {
+	//		framebuffer.drawPixel(current_colon, //The first point is drawn as a pixel because it has no past point
+	//									ST7735_TFT_HEIGHT-GLOBAL_graph_values[0][current_colon],
+	//									ST7735_TFT_GREEN);
+	//	}
+	//	else {
+	//		framebuffer.drawLine(current_colon-1,  //The following points are drawn as lines to prevent gaps between individual points on steep hillsides
+	//									ST7735_TFT_HEIGHT-GLOBAL_graph_values[0][current_colon-1],
+	//									current_colon,
+	//									ST7735_TFT_HEIGHT-GLOBAL_graph_values[0][current_colon],
+	//									ST7735_TFT_GREEN);
+	//	}
+	//}
+
+	framebuffer.display();
+}
+
+
 
 
 uint16_t calc_frequency(uint16_t *adc_values_array, uint16_t adc_values_min_max_mean, uint32_t catch_time_us) {
@@ -148,6 +234,9 @@ uint16_t calc_frequency(uint16_t *adc_values_array, uint16_t adc_values_min_max_
 	uint16_t sample_time_us = catch_time_us/MEASURE_NUM_SAMPLES;
 	uint16_t period_time_us = sample_time_us*adc_mean_values_distances_mean;
 	float freq = (float)1000000/period_time_us;
+
+	if (freq > 2000) freq = 0;
+
 	//Serial.print("period_time_us:");
 	//Serial.print(period_time_us);
 
@@ -159,55 +248,13 @@ uint16_t calc_frequency(uint16_t *adc_values_array, uint16_t adc_values_min_max_
 	//}
 	//Serial.print("\n");
 
+
 	return (uint16_t)freq;
-}
-
-
-void make_graph(uint16_t *adc_values_array, uint16_t adc_values_max) {
-	uint8_t graph_values[GRAPH_WIDTH] = {};
-	uint8_t adc_values_multiplier = 0;
-
-	adc_values_max = adc_values_max/100*10+adc_values_max;
-	if (adc_values_max == 0) { adc_values_max = 1; };
-	adc_values_multiplier = MAX_ADC_VALUE/adc_values_max;
-
-
-	for (uint16_t i=0; i < GRAPH_WIDTH; i++) {
-		graph_values[i] = (adc_values_array[i*GRAPH_WIDTH_DIVIDER+0]*adc_values_multiplier+  //Take 4 values (=GRAPH_WIDTH_DIVIDER)
-												adc_values_array[i*GRAPH_WIDTH_DIVIDER+1]*adc_values_multiplier+ //from the adc values array at a time
-												adc_values_array[i*GRAPH_WIDTH_DIVIDER+2]*adc_values_multiplier+ //and collapse them into one
-												adc_values_array[i*GRAPH_WIDTH_DIVIDER+3]*adc_values_multiplier)
-												/GRAPH_WIDTH_DIVIDER/GRAPH_HEIGHT_DIVIDER;
-		if (graph_values[i] > 50) { graph_values[i] = 50; }
-	}
-
-	for (uint8_t current_colon = GRAPH_X; current_colon < GRAPH_WIDTH; current_colon++) {
-		if (current_colon == 0) {
-			framebuffer.drawPixel(current_colon, //The first point is drawn as a pixel because it has no past point
-										ST7735_TFT_HEIGHT-graph_values[current_colon],
-										ST7735_TFT_GREEN);
-		}
-		else {
-			framebuffer.drawLine(current_colon-1,  //The following points are drawn as lines to prevent gaps between individual points on steep hillsides
-										ST7735_TFT_HEIGHT-graph_values[current_colon-1],
-										current_colon,
-										ST7735_TFT_HEIGHT-graph_values[current_colon],
-										ST7735_TFT_GREEN);
-		}
-	}
-
-
-	//Serial.print("\n");
-	//for (uint8_t current_colon=0; current_colon < GRAPH_WIDTH; current_colon++) {
-	//	Serial.print(graph_values[current_colon]);
-	//	Serial.print("NN");
-	//}
-	//Serial.print("\n");
 }
 
 void measure_flicker() {
 	uint16_t adc_values[MEASURE_NUM_SAMPLES] = {};
-	uint16_t adc_values_max = 1; //to prevent division by zero if we get zeros in the measurement (or after applying the correction).
+	uint16_t adc_values_max = 1; //inital value 1 to prevent division by zero if we get zeros in the measurement (or after applying the correction).
 	uint16_t adc_values_min = MAX_ADC_VALUE;
 	uint16_t adc_values_avg = 0;
 	uint16_t adc_values_min_max_mean = 0;
@@ -287,6 +334,27 @@ void measure_flicker() {
 	//Flicker frequency calculation
 	uint16_t freq = calc_frequency(adc_values, adc_values_min_max_mean, catch_time_us);
 
+	GLOBAL_flicker_gost_uint = flicker_gost_uint;
+	GLOBAL_flicker_simple_uint = flicker_simple_uint;
+	GLOBAL_freq = freq;
+	GLOBAL_adc_values_max = adc_values_max;
+
+
+	//Graph data
+	uint8_t adc_values_multiplier = 0;
+
+	adc_values_max = adc_values_max/100*10+adc_values_max;
+	if (adc_values_max == 0) { adc_values_max = 1; };
+	adc_values_multiplier = MAX_ADC_VALUE/adc_values_max;
+
+	for (uint16_t i=0; i < GRAPH_WIDTH; i++) {
+		GLOBAL_graph_values[0][i] = (adc_values[i*GRAPH_WIDTH_DIVIDER+0]*adc_values_multiplier+  //Take 4 values (=GRAPH_WIDTH_DIVIDER)
+																	adc_values[i*GRAPH_WIDTH_DIVIDER+1]*adc_values_multiplier+ //from the adc values array at a time
+																	adc_values[i*GRAPH_WIDTH_DIVIDER+2]*adc_values_multiplier+ //and collapse them into one
+																	adc_values[i*GRAPH_WIDTH_DIVIDER+3]*adc_values_multiplier)
+																	/GRAPH_WIDTH_DIVIDER/GRAPH_HEIGHT_DIVIDER;
+		if (GLOBAL_graph_values[0][i] > 50) { GLOBAL_graph_values[0][i] = 50; }
+	}
 
 
 	//debug prints
@@ -302,59 +370,18 @@ void measure_flicker() {
 	//Serial.print(flicker_gost);
 	//Serial.print(", flicker_simple:");
 	//Serial.print(flicker_simple);
-
-	framebuffer.fillScreen(ST77XX_BLACK);
-
-	if (flicker_simple >= 0 && flicker_simple <= 5) {
-		draw_asset(&flicker_msg_good_lamp, 0, 0);
-	}
-	else if (flicker_simple > 5 && flicker_simple <= 30) {
-		draw_asset(&flicker_msg_normal_lamp, 0, 0);
-	}
-	else if (flicker_simple > 30) {
-		draw_asset(&flicker_msg_bad_lamp, 0, 0);
-	}
-	else {
-		Serial.println((String)"Flicker simple:"+flicker_simple);
-	}
-	draw_asset(&flicker_rainbow, 0, 39);
-	draw_asset(&arrow, 10, 56);
-	draw_asset(&flicker_text_flicker_level, 0, 61);
-
-
-
-	framebuffer.setCursor(0, 150);
-	framebuffer.setTextColor(ST7735_TFT_WHITE);
-	framebuffer.setTextSize(1);
-
-	//framebuffer.println((String)"Flicker simple:"+flicker_simple_uint);
-	//framebuffer.println((String)"Correction:"+GLOBAL_adc_correction);
-	//framebuffer.println((String)"Average:"+adc_values_avg);
-	//framebuffer.println((String)"Max:"+adc_values_max);
-	//framebuffer.println((String)"Min:"+adc_values_min);
-	//framebuffer.println((String)"Tm:"+((float)catch_time_us/1000)+"ms");
-	framebuffer.println((String)"Freq:"+freq+" Hz");
-	//framebuffer.println((String)"Light:"+LightSensor.GetLightIntensity()+" lx");
-
-	framebuffer.setCursor(40, 80);
-	framebuffer.setTextSize(3);
-	framebuffer.println((String)flicker_gost_uint+"%");
-
-	make_graph(adc_values, adc_values_max);
-
-	framebuffer.display();
 }
 
 
 void measure_light() {
-  uint16_t lux = LightSensor.GetLightIntensity();
-
-	framebuffer.fillScreen(ST77XX_BLACK);
-  framebuffer.setCursor(0, 0);
-	framebuffer.setTextColor(ST7735_TFT_WHITE);
-	framebuffer.setTextSize(1);
-	framebuffer.println((String)"Light:"+lux+" lx");
-	framebuffer.display();
+  uint16_t lum = LightSensor.GetLightIntensity();
+	GLOBAL_lum = lum;
+	//framebuffer.fillScreen(ST7735_TFT_BLACK);
+  //framebuffer.setCursor(0, 0);
+	//framebuffer.setTextColor(ST7735_TFT_WHITE);
+	//framebuffer.setTextSize(0);
+	//framebuffer.println((String)"Light:"+lum+" lx");
+	//framebuffer.display();
 }
 
 void isr() {
@@ -363,10 +390,6 @@ void isr() {
 
 void button_click_handler() {
   Serial.print("Click\n");
-	framebuffer.fillScreen(ST77XX_BLACK);
-	framebuffer.display();
-	draw_asset(&flicker_msg_good_lamp, 0, 0);
-	//measure_flicker();
 }
 
 void free_mem_print() {
@@ -403,7 +426,8 @@ void show_startup_screen_and_get_correction() {
 	//framebuffer.display();
 	//Serial.print(GLOBAL_adc_correction);
 	//Serial.print(", OK.\n");
-	delay(5000);
+	delay(2000);
+	framebuffer.fillScreen(ST7735_TFT_BLACK);
 }
 
 
@@ -427,7 +451,7 @@ void setup(void) {
 
   framebuffer.initR(INITR_BLACKTAB);
 	framebuffer.cp437(true);
-	framebuffer.fillScreen(ST77XX_BLACK);
+	framebuffer.fillScreen(ST7735_TFT_BLACK);
 	framebuffer.display();
 	//
 
@@ -437,9 +461,13 @@ void setup(void) {
 
 	show_startup_screen_and_get_correction();
 
-	//ts.add(0, 200, [&](void *) { measure_light(); }, nullptr, true);
+	ts.add(0, 200, [&](void *) { measure_light(); }, nullptr, true);
+
 	ts.add(1, 100, [&](void *) { measure_flicker(); }, nullptr, true);
-	ts.add(2, 5000, [&](void *) { free_mem_print(); }, nullptr, true);
+	ts.add(2, 100, [&](void *) { render_flicker_screen(); }, nullptr, true);
+
+
+	ts.add(3, 5000, [&](void *) { free_mem_print(); }, nullptr, true);
 
 
 }
